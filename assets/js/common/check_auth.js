5e528cf6e1c0ddd258abccdc5ea22ade75be04ff
AlteaV <90182301+AlteaV@users.noreply.github.com>
2026-03-17 09:59:05 +0530

    Update check_auth.js

--- assets/js/common/check_auth.js
+++ assets/js/common/check_auth.js
@@ -1,5 +1,5 @@
 if (loggedInUser == undefined || loggedInUser == null || loggedInUser == "") {
-  document.location.href = "/auth.html";
+  document.location.href = "/login.html";
 }
 
 function logout() {
